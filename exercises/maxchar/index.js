3f21f3cdb40b0cb20755c557a42e6cf950480b26
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-04-15 11:04:56 -0600

    Add own solution for maxCahrs

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let strValues = str.split('').reduce((strCounts, char) => {
+    if (!strCounts[char]) {
+      strCounts[char] = 0;
+    }
+    strCounts[char]++;
+    return strCounts;
+  }, {});
+  return Object.keys(strValues).reduce((a, b) =>
+    strValues[a] > strValues[b] ? a : b
+  );
+}
 
 module.exports = maxChar;
