b1d983dfe7c087dbf47c83ebccca68088d376b3a
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-04-15 11:29:02 -0600

    Create optimal solution for maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,17 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  let strValues = str.split('').reduce((strCounts, char) => {
-    if (!strCounts[char]) {
-      strCounts[char] = 0;
-    }
-    strCounts[char]++;
-    return strCounts;
+// Optimal?
+
+const maxChar = (str) => {
+  let charMap = str.split('').reduce((charCount, char) => {
+    charCount[char] = charCount[char] + 1 || 1;
+    return charCount;
   }, {});
-  return Object.keys(strValues).reduce((a, b) =>
-    strValues[a] > strValues[b] ? a : b
+  return Object.keys(charMap).reduce((a, b) =>
+    charMap[a] > charMap[b] ? a : b
   );
-}
+};
+
+// Instructor:
+// function maxChar(str) {
+//   let charMap = {};
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+// }
+
+// Own solution:
+// function maxChar(str) {
+//   let strValues = str.split('').reduce((strCounts, char) => {
+//     strCounts[char] ? strCounts[char]++ : (strCounts[char] = 1);
+//     return strCounts;
+//   }, {});
+//   return Object.keys(strValues).reduce((a, b) =>
+//     strValues[a] > strValues[b] ? a : b
+//   );
+// }
 
 module.exports = maxChar;
